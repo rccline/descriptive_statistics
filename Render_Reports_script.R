22d1666489aa374950fe131d6c3f425f6fb05390
Robert <rccline@fastmail.fm>
2021-11-20 11:15:58 -0700

    modify readme.md

---
+++ Render_Reports_script.R
@@ -0,0 +1,6 @@
+## Compiling Reports from R Scripts 
+## https://rmarkdown.rstudio.com/articles_report_from_r_script.html
+
+rmarkdown::render(here::here("docs_descriptive", "2_frequency_tables.Rmd"))
+
+                  
